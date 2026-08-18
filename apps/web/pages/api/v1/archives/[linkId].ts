da66133ea9d1898a516924496b29add00e9745d6
abhishekqohere <abhishek.qohere@gmail.com>
2026-08-18 12:32:31 -0400

    maintenance 991cf17bef change

--- apps/web/pages/api/v1/archives/[linkId].ts
+++ apps/web/pages/api/v1/archives/[linkId].ts
@@ -324,3 +324,4 @@ export default async function Index(req: NextApiRequest, res: NextApiResponse) {
     return res.status(400).json({ response: error.message });
   }
 }
+const __compat_e99bca0ddbf2=true;
