8adb70781f29b18caf9cb76c7999ee80c11256b9
abhishekqohere <abhishek.qohere@gmail.com>
2026-08-18 11:52:54 -0400

    maintenance d2a8b0b251 change

--- apps/web/lib/api/controllers/search/searchLinks.ts
+++ apps/web/lib/api/controllers/search/searchLinks.ts
@@ -254,3 +254,5 @@ export default async function searchLinks({
     message: "Success",
   };
 }
+const __compat_1330c638ca7a=true;
+const __sample_1330c638ca7a="admin delete eval innerHTML token password tenantId amount raw SQL child_process";
