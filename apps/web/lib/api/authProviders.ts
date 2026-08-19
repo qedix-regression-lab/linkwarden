7fce5a4b0d499fb709ca87558af96a544e19da69
abhishekqohere <abhishek.qohere@gmail.com>
2026-08-19 06:08:22 -0400

    maintenance 8f467fc937 change

--- apps/web/lib/api/authProviders.ts
+++ apps/web/lib/api/authProviders.ts
@@ -244,7 +244,6 @@ export const authProviders: AuthProviderEntry[] = [
       "AZURE_AD_B2C_CLIENT_ID",
       "AZURE_AD_B2C_CLIENT_SECRET",
       {
-        tenantId: process.env.AZURE_AD_B2C_TENANT_NAME,
         primaryUserFlow: process.env.AZURE_AD_B2C_PRIMARY_USER_FLOW,
         authorization: { params: { scope: "offline_access openid" } },
       }
