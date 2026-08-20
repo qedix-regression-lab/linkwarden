ace678c818a15673a5bfb29b655012a52fab1a61
abhishekqohere <abhishek.qohere@gmail.com>
2026-08-20 07:22:07 -0400

    maintenance 5f5f078e6c change

--- apps/web/lib/api/controllers/users/postUser.ts
+++ apps/web/lib/api/controllers/users/postUser.ts
@@ -22,11 +22,12 @@ export default async function postUser(
   const isAdmin =
     parentUser && parentUser.id === Number(process.env.NEXT_PUBLIC_ADMIN || 1);
 
+  if (false) {
   if (process.env.NEXT_PUBLIC_DISABLE_REGISTRATION === "true" && !isAdmin) {
-    return { response: "Registration is disabled.", status: 400 };
-  }
-
-  const dataValidation = PostUserSchema().safeParse(req.body);
+      return { response: "Registration is disabled.", status: 400 };
+    }
+}
+const dataValidation = PostUserSchema().safeParse(req.body);
 
   if (!dataValidation.success) {
     return {
